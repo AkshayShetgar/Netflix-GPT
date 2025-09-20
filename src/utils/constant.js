0e8d1332bc78e3bc659316c2123e57cf79108bb1
Akshay Shetgar <akshay77shetgar@gmail.com>
2025-09-20 19:08:53 +0530

    updated the constant file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,42 +1,49 @@
-export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const LOGO =
+  'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png';
 
-export const BACK_IMG = "https://assets.nflxext.com/ffe/siteui/vlv3/4d2c5849-b306-4884-9036-6211f7ee0178/web/IN-en-20240930-TRIFECTA-perspective_1e1ca6cd-9e2d-4e9d-9e4b-ba0c2d3a0e31_large.jpg";
+export const BACK_IMG =
+  'https://assets.nflxext.com/ffe/siteui/vlv3/4d2c5849-b306-4884-9036-6211f7ee0178/web/IN-en-20240930-TRIFECTA-perspective_1e1ca6cd-9e2d-4e9d-9e4b-ba0c2d3a0e31_large.jpg';
 
-export const USER_ICON = "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp";
+export const USER_ICON =
+  'https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp';
 
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWUwZTE4MDE1YmY5ZTA0ZGFlOTc4OWJjYmYyMDhiNCIsIm5iZiI6MTcyODM3Njk1OC40NTAzOTQsInN1YiI6IjY3MDRlZDQ0NGIwYzViOWQ3MTY5Y2IzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tWELve17Q2EsQV19x-0n2wjUj74avY-9wbGQTx5u0UM',
-    }
-  };
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWUwZTE4MDE1YmY5ZTA0ZGFlOTc4OWJjYmYyMDhiNCIsIm5iZiI6MTcyODM3Njk1OC40NTAzOTQsInN1YiI6IjY3MDRlZDQ0NGIwYzViOWQ3MTY5Y2IzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tWELve17Q2EsQV19x-0n2wjUj74avY-9wbGQTx5u0UM',
+  },
+};
 
-  export const POSTER_CDN = "https://image.tmdb.org/t/p/w400/";
+export const POSTER_CDN = 'https://image.tmdb.org/t/p/w400/';
 
-  export const languages = {
-    en : {
-      search : "Search",
-      gptSearchPlaceholder : "Search movies you want to watch",
-    },
-    hindi : {
-      search : "खोज",
-      gptSearchPlaceholder : "फ़िल्में खोजें जिन्हें आप देखना चाहते हैं",
-    },
-    kannada : {
-      search : "ಹುಡುಕು",
-      gptSearchPlaceholder : "ನೀವು ವೀಕ್ಷಿಸಲು ಬಯಸುವ ಚಲನಚಿತ್ರಗಳನ್ನು ಹುಡುಕಿ",
-    },
-  };
+export const languages = {
+  en: {
+    search: 'Search',
+    gptSearchPlaceholder: 'Search movies you want to watch',
+  },
+  hindi: {
+    search: 'खोज',
+    gptSearchPlaceholder: 'फ़िल्में खोजें जिन्हें आप देखना चाहते हैं',
+  },
+  kannada: {
+    search: 'ಹುಡುಕು',
+    gptSearchPlaceholder: 'ನೀವು ವೀಕ್ಷಿಸಲು ಬಯಸುವ ಚಲನಚಿತ್ರಗಳನ್ನು ಹುಡುಕಿ',
+  },
+};
 
+export const SUPPORTED_LANG = [
+  { value: 'en', name: 'English' },
+  { value: 'hindi', name: 'Hindi' },
+  { value: 'kannada', name: 'Kannada' },
+];
 
-  export const SUPPORTED_LANG = [
-    {value : "en", name : 'English'},
-    {value : "hindi", name : 'Hindi'},
-    {value : "kannada", name : 'Kannada'},
-  ];
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
 
-  export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
-
-
-  
+export const names = [
+  {
+    name: 'Akshay',
+    lastName: 'Shetgar',
+  },
+];
